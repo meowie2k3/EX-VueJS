204f3029e82330722027a34b1b9102ec057aa2af
minhhhuydao2003 <daominhhuy.9a1@gmail.com>
2023-09-19 13:03:38 +0700

    little profile setup

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -2,3 +2,17 @@
   <div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Profile',
+  data () {
+    return {
+      user: this.$store.state.user
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
